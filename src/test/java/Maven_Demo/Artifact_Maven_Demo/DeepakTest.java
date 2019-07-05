db107828b726217f89de87c9856b0ca3fd9dd0d1
deepaksjprr <deepak.sharma1@metacube.com>
2019-07-05 16:53:22 +0530

    first commit for git jenkins maven demo

---
+++ src/test/java/Maven_Demo/Artifact_Maven_Demo/DeepakTest.java
@@ -0,0 +1,33 @@
+package Maven_Demo.Artifact_Maven_Demo;
+
+import org.testng.annotations.Test;
+
+public class DeepakTest {
+	
+	
+		@Test
+		public void sum()
+		{
+			System.out.println("this is sum method");
+		}
+		
+		@Test
+		public void subtract()
+		{
+			System.out.println("this is subtract method");
+		}
+		
+		@Test
+		public void multiply()
+		{
+			System.out.println("this is multiply method");
+		}
+		
+		@Test
+		public void division()
+		{
+			System.out.println("this is division method");
+		}	
+	
+
+}
---
+++ src/test/java/Maven_Demo/Artifact_Maven_Demo/DeepakTest2.java
@@ -0,0 +1,32 @@
+package Maven_Demo.Artifact_Maven_Demo;
+
+import org.testng.annotations.Test;
+
+public class DeepakTest2 {
+	
+	@Test
+	public void sum()
+	{
+		System.out.println("this is sum method");
+	}
+	
+	@Test
+	public void subtract()
+	{
+		System.out.println("this is subtract method");
+	}
+	
+	@Test
+	public void multiply()
+	{
+		System.out.println("this is multiply method");
+	}
+	
+	@Test
+	public void division()
+	{
+		System.out.println("this is division method");
+	}	
+
+
+}
